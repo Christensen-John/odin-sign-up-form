c1d99eefd8579969cf726e0780cc893fe65fe19d
Christensen-John <121368802+Christensen-John@users.noreply.github.com>
2023-11-13 14:49:03 +0100

    Add password equality checking and error

--- validationScripts.js
+++ validationScripts.js
@@ -1,5 +1,22 @@
-const form = document.forms["account-info-form"].elements;
-const psInMain = form.namedItem("password");
-const psInConfirm = form.namedItem("password-confirmation");
+// const formElList = document.forms["account-info-form"].elements;
+const form = document.getElementById("account-info-form");
+const psInMain = document.getElementById("password");
+const psInConfirm = document.getElementById("password-confirmation");
 
 //Listeners
+
+//Password comfirmation check
+form.addEventListener("focusout", (event) => {
+	if (
+		event.target.id === "password" ||
+		event.target.id === "password-confirmation"
+	) {
+		if (psInMain.value !== psInConfirm.value) {
+			psInMain.classList.add("pwdMismatch");
+			psInConfirm.classList.add("pwdMismatch");
+		} else if (psInMain.value === psInConfirm.value) {
+			psInMain.classList.remove("pwdMismatch");
+			psInConfirm.classList.remove("pwdMismatch");
+		}
+	}
+});
